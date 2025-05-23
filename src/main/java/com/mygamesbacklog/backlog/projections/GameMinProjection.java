2c7355e64101452163c45010d53bfccafd64daf2
thiago.san <thisantosareis@gmail.com>
2025-05-23 19:13:41 -0300

    Homolog OK

--- src/main/java/com/mygamesbacklog/backlog/projections/GameMinProjection.java
+++ src/main/java/com/mygamesbacklog/backlog/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.mygamesbacklog.backlog.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
